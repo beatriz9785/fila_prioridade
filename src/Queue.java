38a564e2dad4ca2b0af95c4a11d3792667e72191
Beatriz <beatrizschulter@gmail.com>
2021-10-20 21:31:11 -0300

    first commit

---
+++ src/Queue.java
@@ -0,0 +1,23 @@
+public class Queue {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        priorityQueue.enqueueWithPriority(13,5);
+        priorityQueue.enqueueWithPriority(7,2);
+        priorityQueue.enqueueWithPriority(0,0);
+        priorityQueue.enqueueWithPriority(27,3);
+        priorityQueue.enqueueWithPriority(10,11);
+        priorityQueue.enqueueWithPriority(6,24);
+
+        //Array to string
+        priorityQueue.a();
+
+        System.out.println();
+        System.out.println("Priority queue: " + priorityQueue);
+        priorityQueue.dequeue();
+        priorityQueue.dequeue();
+        priorityQueue.dequeue();
+        System.out.println();
+
+        System.out.println("Priority queue: " + priorityQueue);
+    }
+}
